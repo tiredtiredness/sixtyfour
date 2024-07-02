97b747716706cefdf8bfb8138969df1ab73e25f0
tiredtiredness <tiredtiredness@gmail.com>
2024-07-02 03:08:22 +0300

    Try fix no page when reloading error

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom/client';
 import Header from './Header.jsx';
 import Content from './Content.jsx';
 import Footer from './Footer.jsx';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, hashHistory } from 'react-router-dom';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter history={hashHistory}>
       <Header></Header>
       <Content></Content>
       <Footer></Footer>
