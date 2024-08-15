57981909bd1e1b59223aafcf529e917f9bd9d792
tiredtiredness <tiredtiredness@gmail.com>
2024-08-16 00:18:26 +0300

    Add new menu

--- src/Menu.jsx
+++ src/Menu.jsx
@@ -4,9 +4,10 @@ export default function Menu() {
   return (
     <>
       <div className='menu-bar'>
-        <img src='/images/menu/Меню Sixty Four 2024 новое -2.png' alt='' />
-        <img src='/images/menu/Меню Sixty Four 2024 новое -3.png' alt='' />
-        <img src='/images/menu/Меню Sixty Four 2024 новое -4.png' alt='' />
+        <img src='/images/menu/menu1.png' alt='' />
+        <img src='/images/menu/menu2.png' alt='' />
+        <img src='/images/menu/menu3.png' alt='' />
+        <img src='/images/menu/menu4.png' alt='' />
       </div>
     </>
   );
